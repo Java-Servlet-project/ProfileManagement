e2a72ddbe26df692bd830f6e8e62f7ce45b00397
soumitradutta <55148899+soumitradutta@users.noreply.github.com>
2019-12-20 17:53:27 +0530

    Add files via upload

--- src/main/java/org/profilemanagement/service/LoginService.java
+++ src/main/java/org/profilemanagement/service/LoginService.java
@@ -1,13 +1,26 @@
 package org.profilemanagement.service;
 
+import org.profilemanagement.constants.IamConstant;
+
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 public class LoginService {
-    
+
     public boolean login(final HttpServletRequest request) {
         final String userName = request.getParameter("username");
         final String password = request.getParameter("password");
         return ("Admin".equals(userName) && "mypassword".equals(password)) ? Boolean.TRUE : Boolean.FALSE;
     }
 
+    public Cookie loginAndSetCookie(final HttpServletRequest request) {
+        Cookie cookie = null;
+        final String userName = request.getParameter("username");
+        final String password = request.getParameter("password");
+        if ("Admin".equals(userName) && "mypassword".equals(password)) {
+            cookie = new Cookie(IamConstant.LOGIN_COOKIE_NAME, userName);
+            cookie.setMaxAge(120);
+        }
+        return cookie;
+    }
 }
--- src/main/java/org/profilemanagement/service/ProfileService.java
+++ src/main/java/org/profilemanagement/service/ProfileService.java
@@ -1,5 +1,21 @@
 package org.profilemanagement.service;
 
+import org.profilemanagement.constants.IamConstant;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 public class ProfileService {
 
+    public String userProfile(final HttpServletRequest request) {
+        final StringBuffer buffer = new StringBuffer();
+        final Cookie[] cookies = request.getCookies();
+        for (final Cookie cookie : cookies) {
+            if (IamConstant.LOGIN_COOKIE_NAME.equals(cookie.getName())) {
+                buffer.append("<h1>Hello ").append(cookie.getValue()).append(" ! ");
+                buffer.append("This is your profile</h1>");
+            }
+        }
+        return buffer.toString();
+    }
 }
