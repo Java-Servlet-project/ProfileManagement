fd0028150fa5683cfc79949710695b7ce53b2d5a
soumitradutta <55148899+soumitradutta@users.noreply.github.com>
2019-12-23 12:27:11 +0530

    Add files via upload

---
+++ src/main/java/org/profilemanagement/model/Employee.java
@@ -0,0 +1,34 @@
+package org.profilemanagement.model;
+
+public class Employee {
+
+    private String firstName;
+
+    private String middleName;
+
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(final String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(final String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(final String lastName) {
+        this.lastName = lastName;
+    }
+}
